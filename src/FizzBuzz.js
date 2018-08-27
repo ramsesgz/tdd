17faa988691c3b872e325aab64cd8e993243102e
Ramses Goncalves <ramseswd@gmail.com>
2018-08-27 08:53:33 -0300

    Ajustes

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,12 +1,13 @@
 const FizzBuzz = (num) => {
-    debugger;
     num = Number(num);
     if(isNaN(num)) return 'Informe um número válido!';
     if(num == 0 ) return 0;
     if((num % 5 == 0) && (num % 15 === 0)) return 'FizzBuzz';
     if(num % 3 === 0) return 'Fizz';
     if(num % 5 === 0) return 'Buzz';
-    console.log("Teste: ", typeof num === NaN)
+    console.log("Teste: ", typeof num === NaN);
+    console.log("Teste: ", typeof num === "x");
+    
     return num;
 }
 
