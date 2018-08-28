82aa0180eedde3310caeeb8ccf74c854be043b3f
Ramsés <ramses.zinho@globalhitss.com.br>
2018-08-28 16:10:02 -0300

    Bugfix

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,17 +1,18 @@
 const FizzBuzz = (num) => {
-  if (isNaN(num)) {
+  const number = Number(num);
+  if (isNaN(number)) {
     return 'Informe um número válido!';
-  } else if (num === 0) {
+  } else if (number === 0) {
     return 0;
-  } else if ((num % 5 === 0) && (num % 15 === 0)) {
+  } else if ((number % 5 === 0) && (number % 15 === 0)) {
     return 'FizzBuzz';
-  } else if (num % 3 === 0) {
+  } else if (number % 3 === 0) {
     return 'Fizz';
-  } else if (num % 5 === 0) {
+  } else if (number % 5 === 0) {
     return 'Buzz';
   }
 
-  return num;
+  return number;
 };
 
 export default FizzBuzz;
