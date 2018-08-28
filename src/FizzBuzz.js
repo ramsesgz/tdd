7a6e758c165d1a1aaff5e5efb55b345f934301f1
Ramsés <ramses.zinho@globalhitss.com.br>
2018-08-28 15:47:46 -0300

    Code review

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,13 +1,17 @@
 const FizzBuzz = (num) => {
     num = Number(num);
-    if(isNaN(num)) return 'Informe um número válido!';
-    if(num == 0 ) return 0;
-    if((num % 5 == 0) && (num % 15 === 0)) return 'FizzBuzz';
-    if(num % 3 === 0) return 'Fizz';
-    if(num % 5 === 0) return 'Buzz';
-    console.log("Teste: ", typeof num === NaN);
-    console.log("Teste: ", typeof num === "x");
-    
+    if(isNaN(num)){
+        return 'Informe um número válido!';
+    }else if(num == 0 ){
+        return 0;
+    } else if((num % 5 == 0) && (num % 15 === 0)){
+        return 'FizzBuzz';
+    }else if(num % 3 === 0){
+        return 'Fizz';
+    }else if(num % 5 === 0) {
+        return 'Buzz';
+    }
+
     return num;
 }
 
--- tests/FizzBuzz.spec.js
+++ tests/FizzBuzz.spec.js
@@ -11,33 +11,30 @@ import { expect } from 'chai';
 import FizzBuzz from '../src/FizzBuzz';
 
 describe('FizzBuzz', () =>{
-    
+
     it('Should return `Fizz` if {num} is multiple of 3', () => {
         expect(FizzBuzz(3)).to.be.equal('Fizz')
-        expect(FizzBuzz(6)).to.be.equal('Fizz')
-        expect(FizzBuzz(6)).to.be.equal('Fizz')
-
     });
     it('Should return `Buzz` if {num} is multiple of 5', () => {
         expect(FizzBuzz(5)).to.be.equal('Buzz')
         expect(FizzBuzz(10)).to.be.equal('Buzz')
     });
+
     it('Should return `FizzBuzz` if {num} is multiple of 3 and 5', () => {
         expect(FizzBuzz(15)).to.be.equal('FizzBuzz')
     });
+
     it('Should return the number when non-multiple', () => {
         expect(FizzBuzz(2)).to.be.equal(2);
         expect(FizzBuzz(11)).to.be.equal(11);
         expect(FizzBuzz(.1)).to.be.equal(.1);
         expect(FizzBuzz(10.5)).to.be.equal(10.5);
-        expect(FizzBuzz(0)).to.be.equal(0);
-
-
-
+        expect(FizzBuzz('0')).to.be.equal(0);
     });
 
     it('Should return `Informe um número válido!` when `value` is NaN', () => {
         expect(FizzBuzz('a')).to.be.equal('Informe um número válido!');
+        expect(FizzBuzz()).to.be.equal('Informe um número válido!');
     })
-    
+
 })
